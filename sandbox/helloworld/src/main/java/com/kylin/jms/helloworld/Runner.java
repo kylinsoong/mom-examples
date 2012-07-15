ec3ae56efc534f4c1fa78a4d5524dd4f3dd38921
Kylin <songzhiqi_1214@yahoo.com.cn>
2012-07-15 17:12:28 +0800

    change JMS Hello World code

---
+++ sandbox/helloworld/src/main/java/com/kylin/jms/helloworld/Runner.java
@@ -0,0 +1,14 @@
+package com.kylin.jms.helloworld;
+
+import javax.naming.NamingException;
+
+public class Runner {
+
+	public static void main(String[] args) throws Exception {
+		
+		HelloWorlsJMS test = new HelloWorlsJMS();
+		
+		test.test();
+	}
+
+}
